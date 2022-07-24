745a461fa42ad4633916376102231bbbe34e2f21
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-24 16:30:36 -0400

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import About from "../views/About.vue";
+import EventDetails from "../views/EventDetails";
 
 const routes = [
   {
     path: "/",
     name: "EventList",
     component: EventList
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    component: EventDetails,
+    props: true
   },
   {
     path: "/about",
