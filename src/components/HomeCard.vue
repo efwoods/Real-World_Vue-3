1dd7f89fd3660b463b52462236f767d8123c5008
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-23 21:12:52 -0400

    commit

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="event-card"></div>
+<!--Display event data-->
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  // props: {
+  //   msg: String
+  // }
+};
+</script>
+</style>
