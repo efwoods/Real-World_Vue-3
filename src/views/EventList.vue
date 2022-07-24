4487655be376d5c0b58c40db691a074b49979ee6
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-24 15:54:16 -0400

    commit

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,50 +7,28 @@
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
-  name: 'Home',
+  name: 'EventList',
   components: {
     EventCard
   },
   data () {
     return {
-      events: [{
-        id: 5928101,
-        category: 'animal welfare',
-        title: 'Cat Adoption Day',
-        description: ' Find your new feline friend at this event.',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petsAllowed: true,
-        organizer: 'Kat Laydee'
-      },
-      {
-        id: 5928101,
-        category: 'animal welfare',
-        title: 'Cat Adoption Day',
-        description: ' Find your new feline friend at this event.',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petsAllowed: true,
-        organizer: 'Kat Laydee'
-      },
-      {
-        id: 5928101,
-        category: 'animal welfare',
-        title: 'Cat Adoption Day',
-        description: ' Find your new feline friend at this event.',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petsAllowed: true,
-        organizer: 'Kat Laydee'
-      }
-      ]
+     events: null
     }
+  },
+  created () {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
+
 }
 </script>
 
