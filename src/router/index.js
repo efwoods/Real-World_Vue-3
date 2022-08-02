1a2d6073076423c6af4386b853947014d70ca2c3
efwoods <22337004+efwoods@users.noreply.github.com>
2022-08-02 16:14:45 -0400

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,36 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import About from "../views/About.vue";
-import EventDetails from "../views/EventDetails";
-import NotFoundComponent from "@/views/NotFoundComponent"
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import About from '../views/About.vue'
+import EventDetails from '../views/EventDetails'
+import NotFoundComponent from '@/views/NotFoundComponent'
 
 const routes = [
   {
-    path: "/",
-    name: "EventList",
+    path: '/',
+    name: 'EventList',
     component: EventList
   },
   {
-    path: "/event/:id",
-    name: "EventDetails",
+    path: '/event/:id',
+    name: 'EventDetails',
     component: EventDetails,
     props: true
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     component: About
   },
   {
     path: '/:catchAll(.*)',
-    name: "NotFoundComponent",
+    name: 'NotFoundComponent',
     component: NotFoundComponent
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
 
-export default router;
+export default router
