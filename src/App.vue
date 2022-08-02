4ce947a93b37b7d7ad52df8aea8cba511ee36330
efwoods <22337004+efwoods@users.noreply.github.com>
2022-08-02 13:04:37 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{name: 'EventList'}">Events</router-link> |
-      <router-link :to="{name: 'About'}">About</router-link>
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
     </div>
-    
+
     <router-view />
   </div>
 </template>
